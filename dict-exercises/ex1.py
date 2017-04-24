92ffabd82b7255968e2511267d3b44988e2e7346
Ronda Wylie <rondawylie@yahoo.com>
2017-04-24 10:54:21 -0500

    Dict-ex1

---
+++ dict-exercises/ex1.py
@@ -0,0 +1,13 @@
+#exercise 1 dictionaries
+
+phonebook_dict = {
+  'Alice': '703-493-1834',
+  'Bob': '857-384-1234',
+  'Elizabeth': '484-584-2923'
+}
+print("Elizabeth's phone number is: " + phonebook_dict['Elizabeth'])
+
+phonebook_dict['Kareem'] = '938-489-1234'
+del phonebook_dict['Alice']
+phonebook_dict['Bob'] = '968-345-2345'
+print(phonebook_dict)
